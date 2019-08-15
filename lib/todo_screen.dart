09297a9cc8a51a7e8e8d523b42cae7268abc471e
Archit S Maniyath <archit778@gmail.com>
2019-08-15 14:49:32 +0530

    firestore used to add items done

--- lib/todo_screen.dart
+++ lib/todo_screen.dart
@@ -1,14 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _auth =FirebaseAuth.instance;
+final _fireStore = Firestore.instance;
+FirebaseUser loggedUser;
+
 
 class ToDo extends StatefulWidget {
-  static const id = 'ToDoScreen';
   @override
   _ToDoState createState() => _ToDoState();
 }
 
 class _ToDoState extends State<ToDo> {
+  String taskTitle;
+  String taskDesc;
+  final messageTextController1 = TextEditingController();
+  final messageTextController2 = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentUser();
+  }
+
+
+  void getCurrentUser() async{
+    try{
+      final user = await _auth.currentUser();
+      if(user!=null){
+        loggedUser = user;
+      }
+    }
+    catch(e){
+      print(e);
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+          child:Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 3,
+                        child:TextField(
+                          controller: messageTextController1,
+                          onChanged: (value){
+                            taskTitle = value;
+                          },
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                            hintText: 'Enter your title',
+                          ),
+                        ),
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child:TextField(
+                        controller: messageTextController2,
+                        onChanged: (value){
+                          taskDesc = value;
+                        },
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: 'Enter your task ',
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        child: FlatButton(
+                            onPressed: (){
+                              _fireStore.collection('ToDo').document().setData({
+                                'taskTitle':taskTitle,
+                                'taskDesc':taskDesc,
+                                'sender':loggedUser.email,
+                              });
+                              messageTextController1.clear();
+                              messageTextController2.clear();
+                              print('done');
+                            },
+
+                            child: Icon(Icons.add),
+                        ),
+                    ),
+                  ],
+                ),
+                //Display your todo list
+              ],
+            ),
+          ) ),
+    );
   }
 }

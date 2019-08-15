54d27170fe383ec57da0a1c02135288896f7f78d
Archit S Maniyath <archit778@gmail.com>
2019-08-15 12:46:30 +0530

    skeletion screen created

---
+++ lib/todo_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ToDo extends StatefulWidget {
+  static const id = 'ToDoScreen';
+  @override
+  _ToDoState createState() => _ToDoState();
+}
+
+class _ToDoState extends State<ToDo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
